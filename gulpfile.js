69c6f0c14ef5ebc37dbc32a8a396b1b9dfa21265
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-12-12 17:59:46 -0600

    Added dependancies and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+
+var browserSync = require("browser-sync").create();
+
+gulp.task("default", function() {
+	browserSync.init({
+		server: {
+			baseDir: "./app"
+		}
+	});
+});
+
+gulp.task("sass", function() {
+	console.log("sass");
+});
