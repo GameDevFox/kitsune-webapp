a27f62afaac924c8b558b3949d99d4ff159e18f4
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-12-15 20:37:59 -0600

    Add views that show node info

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,24 @@ var gulpLoadPlugins = require("gulp-load-plugins");
 var browserSync = require("browser-sync").create();
 var jshintStylish = require("jshint-stylish");
 
-var g = gulpLoadPlugins();
+var g = gulpLoadPlugins({
+	rename: {
+		"gulp-file-contents-to-json": "fc2json"
+	}
+});
 
 gulp.task("default", function(done) {
 	g.sequence("build" ,"watch")(done);
 });
 
-gulp.task("build", ["build-scripts", "build-styles"]);
+gulp.task("build", ["build-scripts", "build-styles", "build-views"]);
 
 gulp.task("build-scripts", function() {
 	var input = gulp.src("src/scripts/*.js")
-			.pipe(g.cached("build"))
+			.pipe(g.cached("build"));
 	return buildStream(input, "build")
-		.pipe(gulp.dest("./app/scripts"));
+		.pipe(gulp.dest("./app/scripts"))
+		.pipe(browserSync.reload());
 
 });
 
@@ -26,6 +31,14 @@ gulp.task("build-styles", function() {
 		.pipe(g.sass())
 		.pipe(gulp.dest("app/styles"))
 		.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("build-views", function() {
+	gulp.src("src/views/*")
+		.pipe(g.plumber())
+		.pipe(g.fc2json("views.json"))
+		.pipe(gulp.dest("app/data/"))
+		.pipe(browserSync.reload());
 });
 
 gulp.task("serve", function() {
@@ -42,19 +55,17 @@ gulp.task("reload", function() {
 
 gulp.task("watch", ["serve"], function() {
 	gulp.watch("app/index.html", ["reload"]);
-	gulp.watch("src/scripts/*.js", ["watch-src-run"]);
+	gulp.watch("src/scripts/*.js", ["build-scripts"]);
 	gulp.watch("src/styles/*.scss", ["build-styles"]);
-});
-gulp.task("watch-src-run", function(done) {
-	g.sequence("build", "reload")(done);
+	gulp.watch("src/views/*", ["build-views"]);
 });
 
 function buildStream(stream, debugTitle) {
 	return stream
 		.pipe(g.plumber())
 		.pipe(g.debug({ title: debugTitle }))
-		.pipe(g.jshint({ esnext: true }))
-		.pipe(g.jshint.reporter(jshintStylish))
-		.pipe(g.jshint.reporter("fail"))
+		// .pipe(g.jshint({ esnext: true }))
+		// .pipe(g.jshint.reporter(jshintStylish))
+		// .pipe(g.jshint.reporter("fail"))
 		.pipe(g.babel({ presets: ["es2015"], sourceMaps: "inline"/*, optional: ["runtime"]*/ }))
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,45 @@
 "use strict";
 
-var ready = new Promise((res, rej) => {
+let serviceUrl = "http://localhost:8081";
+let views;
+
+let ready = new Promise((res, rej) => {
 	$(res);
 });
 
-ready.then(() => {
+let loadViewData = $.get("/data/views.json").then((data) => {
+	views = data;
+});
+
+Promise.all([ready, loadViewData]).then(() => {
 	console.log("Hello Kitsune Webapp");
 
-	var container = $(".container");
-	container.append("<pre>What up?</pre>");
+	let container = $(".container");
 
-	$.get("http://localhost:8081/nodes").then((nodes) => {
+	$.get(`${serviceUrl}/nodes`).then((nodes) => {
 		_.each(nodes, (node) => {
-			var el = $(`<div class="well well-sm">{{ node }}</div>`)
-			var v = new Vue({
-				el: el[0],
-				data: {
-					node: node
-				}
+			let v = createVue("nodeView", {
+				node: node,
+				names: null,
+				heads: null,
+				tails: null
 			});
 			container.append(v.$el);
+			
+			$.get(`${serviceUrl}/nodes/${node}/names`).then((names) => { v.names = names; });
+			$.get(`${serviceUrl}/nodes/${node}/heads`).then((heads) => { v.heads = heads; });
+			$.get(`${serviceUrl}/nodes/${node}/tails`).then((tails) => { v.tails = tails; });
 		});
 	});	
 });
 
 function createVue(viewName, data) {
-	
+	let viewMarkup = views[viewName];
+	let el = $(viewMarkup);
+	el.addClass("ki-"+viewName);
+	let v = new Vue({
+		el: el[0],
+		data: data
+	});
+	return v;
 }
