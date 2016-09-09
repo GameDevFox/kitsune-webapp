c18b4f23ec8af38f1686185e84aca67eb36d79d6
Edward Nicholes Jr <GameDevFox@gmail.com>
2016-09-08 20:10:46 -0500

    Refactor kitsune module files

--- src/scripts/kitsune/components/node-details.component.js
+++ src/scripts/kitsune/components/node-details.component.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-    let mod = angular.module("kitsune", ["ngMaterial", "ui.router"]);
+    var mod = angular.module("kitsune");
 
     mod.component("nodeDetails", {
         templateUrl: "templates/node-details.html",
@@ -62,24 +62,6 @@
         },
         controllerAs: "vm",
         bindings: { node: "<" }
-    });
-
-    // CONFIG //
-    mod.constant("kitsuneUrl", "http://localhost:8080/");
-
-    mod.config(function($mdThemingProvider, $urlRouterProvider, $stateProvider) {
-        $mdThemingProvider
-            .theme('default')
-            .primaryPalette('blue')
-            .accentPalette('red');
-
-        $urlRouterProvider.otherwise('/node/7f82d45a6ffb5c345f84237a621de35dd8b7b0e3');
-
-        $stateProvider.state('node-view', {
-            url: "/node/:id",
-            templateUrl: 'templates/main.html',
-            controller: "kitsune as vm"
-        });
     });
 
 })(angular);
